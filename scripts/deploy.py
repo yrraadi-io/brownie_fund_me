5c57cdd493e97dd2307c34944d93f39fa3434d4c
Yash Rathod <yrraadi@gmail.com>
2021-11-21 23:11:24 -0600

    commit 2

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,25 @@
-from brownie import FundMe
-from scripts.helpful_scripts import get_account
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 
 
 def deploy_fund_me():
     account = get_account()
-    # Pass price feed address to FundMe contract
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
     fund_me = FundMe.deploy(
-        "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        price_feed_address,
         {"from": account},
-        publish_source=True,
+        publish_source=config["networks"][network.show_active()].get("verify"),
     )
     print(f"Contract deployed to {fund_me.address}")
 
